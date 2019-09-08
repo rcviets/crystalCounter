c5762486ba1f225beccbdaab26593bb678e34ae9
Randamus <rcviets@gmail.com>
2019-09-08 13:29:53 -0600

    assign random hidden values to images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-//generate random winning number
-var winningNum = [];
+    //generate random winning number
+    var winningNum = [];
 
-function getRandomArbitrary(min, max) {
-    winningNum = Math.floor(Math.random() * (121-19) + 19);
-    $("#winningNum").text(winningNum);
-}
-getRandomArbitrary()
+    function getRandomArbitrary(min, max) {
+        winningNum = Math.floor(Math.random() * (121 - 19) + 19);
+        $("#winningNum").text(winningNum);
+    }
+    getRandomArbitrary()
 
-//generate round specific button values
+    //generate round specific button values
+    var btn1 = Math.floor(Math.random() * (12 - 1) + 1);
+    var btn2 = Math.floor(Math.random() * (12 - 1) + 1);
+    var btn3 = Math.floor(Math.random() * (12 - 1) + 1);
+    var btn4 = Math.floor(Math.random() * (12 - 1) + 1);
 
-//button onclick events
-$(".col-md-3").on("click", function(){
+    console.log(btn1, btn2, btn3, btn4)
+    //button onclick events
+    $(".col-md-3").on("click", function () {
 
-console.log("click")
-});
+        console.log("click")
+    });
 
-//update the score counter
+    //update the score counter
 
-//check for a win (if score and winning number match)
+    //check for a win (if score and winning number match)
 
-//check for a loss (if score is greater than winning number)
+    //check for a loss (if score is greater than winning number)
 
-//game reset
+    //game reset
 
-//update wins and losses
+    //update wins and losses
 
 });
