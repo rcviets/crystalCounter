e8f3862ca1d1cb72589543249e80d1442c2acb45
Randamus <rcviets@gmail.com>
2019-09-08 10:02:05 -0600

    jQuery outline

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+//generate random winning number
+
+//generate round specific button values
+
+//button onclick events
+
+//update the score counter
+
+//check for a win (if score and winning number match)
+
+//check for a loss (if score is greater than winning number)
+
+//game reset
+
+//update wins and losses
+
+});
