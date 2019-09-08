bb709cfd3a6e5d8db233aa561e123ff8296be797
Randamus <rcviets@gmail.com>
2019-09-08 12:44:45 -0600

    generate random winning number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 
 //generate random winning number
+var winningNum = [];
+
+function getRandomArbitrary(min, max) {
+    winningNum = Math.floor(Math.random() * (121-19) + 19);
+    $("#winningNum").text(winningNum);
+}
+getRandomArbitrary()
 
 //generate round specific button values
 
 //button onclick events
 $(".col-md-3").on("click", function(){
 
-    console.log("click")
+console.log("click")
 });
 
 //update the score counter
