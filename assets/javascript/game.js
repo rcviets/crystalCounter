021526f7aca12644933f2ad3e965d843a289cace
Randamus <rcviets@gmail.com>
2019-09-10 21:12:06 -0600

    fixed win/loss bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,55 +8,63 @@ $(document).ready(function () {
 
 
     //generate random winning number
-    function getRandomArbitrary(min, max) {
+    function startGame() {
         winningNum = Math.floor(Math.random() * (121 - 19) + 19);
         $("#winningNum").text(winningNum);
+
+
+        //generate round specific button values
+        var btn1 = Math.floor(Math.random() * (12 - 1) + 1);
+        var btn2 = Math.floor(Math.random() * (12 - 1) + 1);
+        var btn3 = Math.floor(Math.random() * (12 - 1) + 1);
+        var btn4 = Math.floor(Math.random() * (12 - 1) + 1);
+
+        console.log(btn1, btn2, btn3, btn4)
+
+        //button onclick events & score counter
+        $("#btn1").on("click", function () {
+            playerScore = parseInt(playerScore + btn1);
+            $("#score").text(playerScore);
+            endGame();
+        });
+
+        $("#btn2").on("click", function () {
+            playerScore = parseInt(playerScore + btn2);
+            $("#score").text(playerScore);
+            endGame();
+        });
+
+        $("#btn3").on("click", function () {
+            playerScore = parseInt(playerScore + btn3);
+            $("#score").text(playerScore);
+            endGame();
+        });
+
+        $("#btn4").on("click", function () {
+            playerScore = parseInt(playerScore + btn4);
+            $("#score").text(playerScore);
+            endGame();
+        });
     }
-    getRandomArbitrary()
-
-    //generate round specific button values
-    var btn1 = Math.floor(Math.random() * (12 - 1) + 1);
-    var btn2 = Math.floor(Math.random() * (12 - 1) + 1);
-    var btn3 = Math.floor(Math.random() * (12 - 1) + 1);
-    var btn4 = Math.floor(Math.random() * (12 - 1) + 1);
-
-    console.log(btn1, btn2, btn3, btn4)
-
-    //button onclick events & score counter
-    $("#btn1").on("click", function () {
-        playerScore = parseInt(playerScore + btn1);
-        $("#score").text(playerScore);
-    });
-
-    $("#btn2").on("click", function () {
-        playerScore = parseInt(playerScore + btn2);
-        $("#score").text(playerScore);
-    });
-
-    $("#btn3").on("click", function () {
-        playerScore = parseInt(playerScore + btn3);
-        $("#score").text(playerScore);
-    });
-
-    $("#btn4").on("click", function () {
-        playerScore = parseInt(playerScore + btn4);
-        $("#score").text(playerScore);
-    });
 
     //check for a win or loss
     function endGame() {
         if (playerScore === winningNum) {
             wins++
-           // reset()
+            playerScore = 0;
             $("#win").text(wins);
+            $("#score").text(playerScore);
+            startGame();
         }
-        else {
+        else if (playerScore > winningNum) {
             losses++
-           // reset()
+            playerScore = 0;
             $("#loss").text(losses);
+            $("#score").text(playerScore);
+            startGame();
         }
     }
-    endGame()
+    startGame()
 
     //game reset
 
