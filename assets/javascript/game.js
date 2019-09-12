5118ae19d05474066a930c2d495e50458291e4d8
Randamus <rcviets@gmail.com>
2019-09-11 18:25:39 -0600

    win/loss alert div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,47 +6,46 @@ $(document).ready(function () {
     var wins = [];
     var losses = [];
 
+    var btn1 = 0;
+    var btn2 = 0;
+    var btn3 = 0;
+    var btn4 = 0;
 
-    //generate random winning number
+    //generate random winning number & generate round specific button values
     function startGame() {
         winningNum = Math.floor(Math.random() * (121 - 19) + 19);
         $("#winningNum").text(winningNum);
 
-
-        //generate round specific button values
-        var btn1 = Math.floor(Math.random() * (12 - 1) + 1);
-        var btn2 = Math.floor(Math.random() * (12 - 1) + 1);
-        var btn3 = Math.floor(Math.random() * (12 - 1) + 1);
-        var btn4 = Math.floor(Math.random() * (12 - 1) + 1);
-
-        console.log(btn1, btn2, btn3, btn4)
-
-        //button onclick events & score counter
-        $("#btn1").on("click", function () {
-            playerScore = parseInt(playerScore + btn1);
-            $("#score").text(playerScore);
-            endGame();
-        });
-
-        $("#btn2").on("click", function () {
-            playerScore = parseInt(playerScore + btn2);
-            $("#score").text(playerScore);
-            endGame();
-        });
-
-        $("#btn3").on("click", function () {
-            playerScore = parseInt(playerScore + btn3);
-            $("#score").text(playerScore);
-            endGame();
-        });
-
-        $("#btn4").on("click", function () {
-            playerScore = parseInt(playerScore + btn4);
-            $("#score").text(playerScore);
-            endGame();
-            console.log(btn4)
-        });
+        btn1 = Math.floor(Math.random() * (12 - 1) + 1);
+        btn2 = Math.floor(Math.random() * (12 - 1) + 1);
+        btn3 = Math.floor(Math.random() * (12 - 1) + 1);
+        btn4 = Math.floor(Math.random() * (12 - 1) + 1);
     }
+
+    //button onclick events & score counter
+    $("#btn1").on("click", function () {
+        playerScore = parseInt(playerScore + btn1);
+        $("#score").text(playerScore);
+        endGame();
+    });
+
+    $("#btn2").on("click", function () {
+        playerScore = parseInt(playerScore + btn2);
+        $("#score").text(playerScore);
+        endGame();
+    });
+
+    $("#btn3").on("click", function () {
+        playerScore = parseInt(playerScore + btn3);
+        $("#score").text(playerScore);
+        endGame();
+    });
+
+    $("#btn4").on("click", function () {
+        playerScore = parseInt(playerScore + btn4);
+        $("#score").text(playerScore);
+        endGame();
+    });
 
     //check for a win or loss
     function endGame() {
@@ -55,6 +54,7 @@ $(document).ready(function () {
             playerScore = 0;
             $("#win").text(wins);
             $("#score").text(playerScore);
+            $("#alert").html("YOU WIN!");
             startGame();
         }
         else if (playerScore > winningNum) {
@@ -62,13 +62,9 @@ $(document).ready(function () {
             playerScore = 0;
             $("#loss").text(losses);
             $("#score").text(playerScore);
+            $("#alert").html("YOU LOSE!");
             startGame();
         }
     }
     startGame()
-
-    //game reset
-
-    //update wins and losses
-
 });
